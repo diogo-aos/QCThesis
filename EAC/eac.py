5bfb864e77500266d6f6120afa60e06eb44bec2b
Diogo <akins.daos@gmail.com>
2015-04-25 10:41:59 +0100

    eac knn. eac notebook testing

--- EAC/eac.py
+++ EAC/eac.py
@@ -14,23 +14,28 @@ TODO:
 - link everything
 - add sanity checks on number of samples of partitions
 - robust exception handling
+- fix centroid-based prototype creation
+- convert dissimilarity matrix to float32
 """
 
 import numpy as np
 from scipy.cluster.hierarchy import linkage
 from scipy.spatial.distance import squareform
-
+from sklearn.neighbors import NearestNeighbors
+from K_Means3 import K_Means
 
 class EAC():
 
-	def __init__(self,nsamples):
+	def __init__(self,nsamples,data=None):
 		self._N = nsamples
 		self._assoc_mode = None
 		self._prot_mode = None
 
+		self.data = data
 		self.npartitions = 0
 
-	def fit(self,ensemble,files=False,assoc_mode="full",prot_mode="random",nprot=None,link='single'):
+	def fit(self,ensemble,files=False,assoc_mode="full",prot_mode="random",
+			nprot=None,link='single'):
 		"""
 		ensemble 		: list of partitions; each partition is a list of arrays (clusterings);
 						  each array contains the indices of the cluster's data;  if files=True,
@@ -47,7 +52,7 @@ class EAC():
 		if self._prot_mode is None:
 			self._prot_mode = prot_mode
 
-
+		# create co-association matrix
 		self._coassoc = self._create_coassoc(assoc_mode,self._N,nprot=nprot)
 
 		if assoc_mode is not "full":
@@ -58,16 +63,17 @@ class EAC():
 				self._assoc_mode="other"
 
 
-			self._build_prototypes(nprot=nprot,mode=prot_mode)
+			self._build_prototypes(nprot=nprot,mode=prot_mode, data=self.data)
 
+		# received names of partition files
 		if files:
 			for partition_file in ensemble:
-				partition = self._readPartition(partition_file)
-				self._update_coassoc_matrix(partition)
-
+				partition = self._readPartition(partition_file) # read partition from file
+				self._update_coassoc_matrix(partition) # update co-association matrix
+		# received partitions
 		else:
 			for partition in ensemble:
-				self._update_coassoc_matrix(partition)
+				self._update_coassoc_matrix(partition) # update co-association matrix
 
 		# transform votes in dissimilarity matrix
 		self._diss_assoc = self._coassoc / self.npartitions # normalize
@@ -76,7 +82,7 @@ class EAC():
 		# apply linkage
 		if assoc_mode is "full":
 		
-			self.hierarchy = self._apply_linkage(self,self._diss_assoc,link)
+			self.hierarchy = self._apply_linkage(self._diss_assoc,link)
 
 		pass
 
@@ -124,16 +130,23 @@ class EAC():
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-	def _build_prototypes(self,nprot=None,mode="random"):
+	def _build_prototypes(self,nprot=None,mode="random",data=None):
 		if nprot == None:
 			nprot = np.sqrt(self._N)
 
 		if mode == "random":
 			self.k_labels = self._build_random_prototypes(nprot,self._N)
+
 		elif mode == "knn":
-			raise Exception("_build_prototypes mode Not implemented.")
+			if data is None:
+				raise Exception("Data needs to be set for this method of choosing prototypes.")
+			self.k_neighbours = self._build_knn_prototypes(nprot,data)
+
 		elif mode == "other":
-			raise Exception("_build_prototypes mode Not implemented.")
+			if data is None:
+				raise Exception("Data needs to be set for this method of choosing prototypes.")
+			self.k_labels = self._build_k_prototypes(nprot,data)
+
 		else:
 			validValues=("random","knn","other")
 			raise ValueError("Mode value should be from the list:\t" + str(validValues))
@@ -146,12 +159,37 @@ class EAC():
 	def _build_knn_prototypes(self,nprot,data):
 		# K-Nearest Neighbours algorithm
 		# should return an NxK array with the labels
-		pass
+		nneigh=nprot+1 #first neighbour is the point itself, it gets discarded afterwards
 
-	def _build_k_prototypes(self,nprot,nsamples):
+		# Minkowski distance is a generalization of Euclidean distance and is equivelent to it for p=2
+		neigh = NearestNeighbors(n_neighbors=nneigh, radius=1.0, algorithm='auto', leaf_size=30, metric='minkowski', p=2)
+		neigh.fit(data)
+
+		k_indices = neigh.kneighbors(X=data,return_distance=False)
+		k_indices = k_indices[:,1:] # discard first neighbour
+
+		return k_indices
+
+	def _build_k_prototypes(self,nprot,data):
 		# K-Means / K-Medoids algorithm
 		# should return a N-length array with he indices of the chosen data
-		pass
+	    grouper = K_Means()
+	    grouper._centroid_mode = "index"
+	    grouper.fit(data, nprot, iters=300, mode="cuda", cuda_mem='manual',tol=1e-4,max_iters=300)
+	    centroids = grouper.centroids
+
+	    nclusters = centroids.shape[0]
+
+	    # TODO - very inefficient
+	    k_labels = np.zeros(nclusters,dtype=np.int32)
+
+	    for k in xrange(nclusters):
+	    	dist = data - centroids[k]
+	    	dist = dist ** 2
+	    	dist = dist.sum(axis=1)
+
+	    	k_labels[k] = dist.argmin()
+		return k_labels
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
@@ -224,6 +262,9 @@ class EAC():
 				# in1d checks common values between two 1-D arrays (a,b) and returns boolean array
 				# with the shape of a with value True on the indices of common values
 				k_in_cluster = np.where(np.in1d(k_labels,n_in_cluster))
+				
+	            if k_in_cluster.size == 0:
+	                continue
 
 				# this indexing selects the rows and columns specified by n_in_cluster and k_in_cluster
 				assoc_mat[n_in_cluster[:,np.newaxis],k_in_cluster] += 1 # np.newaxis is alias for None
@@ -247,11 +288,40 @@ class EAC():
 				# update row j of matrix
 				for j in n_in_cluster:
 					# all prototypes in cluster - columns to select
-					k_in_cluster = np.where(np.in1d(n_in_cluster,k_neighbours[j]))
+					k_in_cluster = np.where(np.in1d(k_neighbours[j],n_in_cluster))
+
+		            if k_in_cluster.size == 0:
+		                continue
 
 					# this indexing selects the rows and columns specified by n_in_cluster and k_in_cluster
 					assoc_mat[j,k_in_cluster] += 1 # np.newaxis is alias for None
+		pass
 
+	def _update_coassoc_knn_sparse(assoc_mat,clusters,k_neighbours):
+	    """
+	    Updates an NxK co-association matrix.
+	    k_neighbours is an NxK array where the k-th element of the i-th row is the index of a data point 
+	    that corresponds to the k-th nearest neighbour of the i-th data point. That neighbour is the k-th
+	    prototype of the i-th data point.
+	    """
+	    nclusters = len(clusters)
+	    for i in xrange(nclusters):
+
+	        if clusters[i].shape > 1:
+
+	            # all data points in cluster - rows to select
+	            n_in_cluster = clusters[i]
+
+	            # update row j of matrix
+	            for j in n_in_cluster:
+	                # all prototypes in cluster - columns to select
+	                # column indices corresponding to the K-prototypes	                
+	                k_in_cluster = n_in_cluster[np.in1d(n_in_cluster,k_neighbours[j])]
+	                
+	                # this indexing selects the rows and columns specified by n_in_cluster and k_in_cluster
+	                #assoc_mat[j,k_in_cluster] += np.ones_like(k_in_cluster)
+	                assoc_mat[k_in_cluster[:,np.newaxis],k_in_cluster] += np.ones_like((k_in_cluster.size,k_in_cluster.size))
+	        pass
 			
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
