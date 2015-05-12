0ae4e616a8fc1000561220b82e3ccdc7837fff5d
Diogo <akins.daos@gmail.com>
2015-05-12 10:37:25 +0100

    from script

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+git status
 git add -A
 git commit -a -m "$1"
 git push
 
-cp $HOME/workspace/thesis_writing/latex/thesis.pdf $HOME/workspace/thesis_website/output/docs/
+cp -v $HOME/workspace/thesis_writing/latex/thesis.pdf $HOME/workspace/thesis_website/output/docs/
