a4087f60498eb90d5f1c7b29407e147f1600fc09
Diogo <akins.daos@gmail.com>
2015-05-12 12:00:04 +0100

    methodology

--- update.sh
+++ update.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+cd $HOME/workspace/thesis_writing
+
 git status
 git add -A
 git commit -a -m "$1"
 git push
 
-cp -v $HOME/workspace/thesis_writing/latex/thesis.pdf $HOME/workspace/thesis_website/output/docs/
+cp -v $HOME/workspace/thesis_writing/latex/build/thesis.pdf $HOME/workspace/thesis_website/output/docs/
+
+cd $HOME/workspace/thesis_website/output
+
+git status
+git add docs/*
+git commit -m "thesis pdf update" docs/*
+git push
